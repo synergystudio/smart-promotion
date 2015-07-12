08e7749c43c3a1f73569a0be26f267eacfd41ebc
Sudjarit Puatham <BLUE_FANTA@Sudjarits-iMac.local>
2015-07-12 19:18:17 +0700

    Initial Commit

---
+++ smart-promotion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smart-promotion
+//
+//  Created by Sudjarit Puatham on 7/12/2558 BE.
+//  Copyright (c) 2558 Sudjarit Puatham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
